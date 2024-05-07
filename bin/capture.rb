4e9dcf93e2b4247a1b95e27f74cdab9625bf0449
tatsuya.nakai <15g079nt@gmail.com>
2024-05-07 16:07:42 +0900

    first commit

---
+++ bin/capture.rb
@@ -0,0 +1,109 @@
+require 'byebug'
+require "csv"
+require 'json'
+require 'RMagick'
+require 'selenium-webdriver'
+require 'uri'
+
+def setup_driver
+  options = Selenium::WebDriver::Options.chrome
+  options.add_argument('--headless')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--window-size=1280,800')
+
+  puts "Setting up the driver..."
+  Selenium::WebDriver.for(:chrome, options: options)
+end
+
+def target_urls
+  puts "Reading URLs from CSV file..."
+  puts ''
+
+  csv = CSV.read('config/urls.csv')
+  csv.flatten
+end
+
+def get_domain(url)
+  uri = URI.parse(url)
+  scheme = uri.scheme
+  host = uri.host
+  "#{scheme}://#{host}/"
+end
+
+def load_cookies(url, driver)
+  puts "  Loading cookies for: #{url}"
+  domain = get_domain(url)
+  driver.navigate.to(domain)
+
+  cookies = JSON.parse(File.read('config/cookies.json'))
+  cookies.select! { |cookie| domain.include?(cookie['domain']) }
+  cookies.each do |cookie|
+    data = {
+      name: cookie['name'],
+      value: cookie['value'],
+      path: cookie['path'],
+      domain: cookie['domain'],
+      secure: cookie['secure'],
+      http_only: cookie['httpOnly']
+    }
+    data[:expires] = Time.at(cookie['expirationDate']) if cookie['expirationDate']
+    driver.manage.add_cookie(data)
+  end
+
+  puts "  Cookies loaded and browser refreshed."
+  driver.navigate.refresh
+end
+
+def add_pathstamp(image_path, url)
+  puts "  Annotating image with URL stamp..."
+  image = Magick::Image.read(image_path).first
+  draw = Magick::Draw.new
+
+  draw.annotate(image, 0, 0, 10, 10, url) do |options|
+    options.pointsize = 32
+    options.fill = 'blue'
+    options.stroke = 'white'
+    options.gravity = Magick::NorthWestGravity
+  end
+  image.write(image_path)
+end
+
+def save_full_screenshot(driver, url)
+  puts "  Capturing full screenshot for: #{url}"
+  timestamp = Time.now.strftime('%Y-%m-%d %H.%M.%S')
+  filename = "スクリーンショット #{timestamp}.png"
+  image_path = "screenshots/#{filename}"
+
+  driver.get(url)
+  sleep(3)
+
+  total_width = driver.execute_script("return document.body.offsetWidth")
+  total_height = driver.execute_script("return document.body.scrollHeight")
+  driver.manage.window.resize_to(total_width, total_height)
+
+  driver.save_screenshot(image_path)
+  add_pathstamp(image_path, url)
+  puts "  Screenshot saved: #{image_path}"
+  puts ''
+end
+
+def capture_screenshot
+  driver = setup_driver
+  urls = target_urls
+
+  urls.each_with_index do |url, index|
+    puts "  Processing #{index + 1}/#{urls.length}: #{url}"
+    begin
+      load_cookies(url, driver)
+      save_full_screenshot(driver, url)
+    rescue => e
+      puts "  [Error] can't capture on #{url}  error: #{e}"
+      next
+    end
+  end
+    
+  driver.quit
+  puts "All screen captures are completed."
+end
+
+capture_screenshot
